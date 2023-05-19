80395869a0c89e27795d942a4f4bf94cdc02c690
Shuvendu Singha <108450702+Sagittarius17@users.noreply.github.com>
2023-05-19 17:44:21 +0000

    function added

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -29,7 +29,7 @@ const Welcome = () => {
   return (
     <section id='home' className="flex flex-col items-center justify-center h-screen md:h-screen bg-[#000]">
       {/* <h2 className="flex relative w-[100%] -top-[165px] text-4xl text-white justify-center font-bold border-t-8 border-l-8 border-r-8 border-solid border-[#a0ff00] pt-6">WELCOME</h2> */}
-      <h1 className='sticky top-0 text-[#ff3030] text-center'>&#9888; This website is under construction! Please use laptop browser or active desktop-mode on mobile for better view.</h1>
+      <h1 className='sticky top-0 text-[#ff3030] text-center'>&#9888; This website is under construction! Please use laptop browser or active desktop-mode on mobile browser for better view.</h1>
       <h1 className="flex relative w-[100%] bottom-[125px] text-6xl text-white justify-center font-bold">WELCOME</h1>
       <div className="relative text-black w-[100%] font-bold border-2 border-solid border-white text-center bg-green-500 lg:p-2 p-0">You viewed this webpage {viewCount} times.</div>
       <div className="m-0 p-0">
@@ -37,7 +37,7 @@ const Welcome = () => {
           Hello and welcome to my portfolio! I am thrilled to have you here and to share my work with you.
           This portfolio represents my passion, creativity, and dedication to my craft. Whether you are a potential client,
           colleague, friends or just curious about my work, I hope that you will find this portfolio informative and engaging.
-          Thank you for taking the time to visit, and I look forward to connecting with you soon.
+          Thank you for taking the time to visit, and I look forward to  <a href="#social-media" className='text-fuchsia-500'>connect</a> with you.
         </p>
       </div>
     </section>
