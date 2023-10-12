1dbdbb3f52516ede0aa952b65a803820a0907f1e
shuvendu <108450702+Sagittarius17@users.noreply.github.com>
2023-10-12 23:19:08 +0530

    new project link added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,120 +2,90 @@ import React, { useState } from "react";
 import logo from "../assets/images/lambda.png";
 
 function Navbar() {
-  const scrollToSection = (id) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-  const menuHighlight = (event) => {
-    // Remove border from all menu items
+    const scrollToSection = (id) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+    const menuHighlight = (event) => {
+        // Remove border from all menu items
 
-    const menuItems = document.getElementsByClassName("btn");
-    for (let i = 0; i < menuItems.length; i++) {
-      menuItems[i].classList.remove("active");
-    }
+        const menuItems = document.getElementsByClassName("btn");
+        for (let i = 0; i < menuItems.length; i++) {
+            menuItems[i].classList.remove("active");
+        }
 
-    // Apply border to clicked menu item
-    const menuItem = event.target.closest(".btn");
-    menuItem.classList.add("active");
-  };
+        // Apply border to clicked menu item
+        const menuItem = event.target.closest(".btn");
+        menuItem.classList.add("active");
+    };
 
-  // Mobile view menu open/close
-  const [isExpanded, setIsExpanded] = useState(false);
+    // Mobile view menu open/close
+    const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleNavbar = () => {
-    console.log("okkkkk");
-    setIsExpanded(!isExpanded);
-  };
+    const toggleNavbar = () => {
+        console.log("okkkkk");
+        setIsExpanded(!isExpanded);
+    };
 
-  return (
-    <nav className="fixed z-10 sm:w-full sm:px-2 bg-white bg-opacity-10 backdrop-blur-md drop-shadow-lg">
-      <div className="flex flex-wrap justify-between items-center mx-5">
-        <a
-          href="#home"
-          className="flex relative lg:bottom-0 md:bottom-[40px] items-center"
-        >
-          <img src={logo} className="mr-3 h-6 sm:h-9" alt="Logo" />
-          <span className="flex self-center text-2xl font-semibold whitespace-nowrap dark:text-purple-300">
-            <h1 className="self-center text-2xl font-semibold whitespace-nowrap dark:text-yellow-400">
-              Shuvendu
-            </h1>
-            Singha
-          </span>
-        </a>
-        <div className="flex">
-          <button
-            data-collapse-toggle="#nav_menu"
-            onClick={toggleNavbar}
-            type="button"
-            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-search"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open menu</span>
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <div id="nav_menu" className="flex w-full md:w-auto">
-          <ul className="flex lg:flex-row lg:h-[70px] lg:items-center lg:justify-start lg:left-0 sm:relative sm:left-[77%] sm:flex-col sm:space-x-8 sm:mt-0 sm:leading-9 sm:font-medium sm:items-end ">
-            <li className="btn">
-              <a
-                href="#home"
-                onClick={(event) => menuHighlight(event)}
-                className="block text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white 
-                                dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Home
-              </a>
-            </li>
-            <li className="btn">
-              <a
-                href="#skills"
-                onClick={(event) => menuHighlight(event)}
-                className="block text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white 
-                                dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Skills
-              </a>
-            </li>
-            <li className="btn">
-              <a
-                href="#projects"
-                onClick={(event) => menuHighlight(event)}
-                className="block text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white 
-                                dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Works
-              </a>
-            </li>
-            <li className="btn">
-              <a
-                href="#pastworks"
-                onClick={(event) => menuHighlight(event)}
-                className="block text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white 
-                                dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Past Work Ex.
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="fixed z-10 sm:w-full sm:px-2 bg-white bg-opacity-10 backdrop-blur-md drop-shadow-lg">
+            <div className="flex flex-wrap justify-between items-center mx-5">
+                <a
+                    href="#home"
+                    className="flex relative lg:bottom-0 md:bottom-[40px] items-center">
+                    <img src={logo} className="mr-3 h-6 sm:h-9" alt="Logo" />
+                    <span className="flex self-center text-2xl font-semibold whitespace-nowrap dark:text-purple-300">
+                        <h1 className="self-center text-2xl font-semibold whitespace-nowrap dark:text-yellow-400">
+                            Shuvendu
+                        </h1>
+                        Singha
+                    </span>
+                </a>
+                <div id="nav_menu" className="flex w-full md:w-auto">
+                    <ul className="flex lg:flex-row lg:h-[70px] lg:items-center lg:justify-start lg:left-0 sm:relative sm:gap-4 sm:left-[77%] sm:flex-col sm:space-x-8 sm:mt-0 sm:leading-9 sm:font-medium sm:items-end ">
+                        <li className="btn">
+                            <a
+                                href="#home"
+                                onClick={(event) => menuHighlight(event)}
+                                className="block text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white 
+                                dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Home
+                            </a>
+                        </li>
+                        <li className="btn">
+                            <a
+                                href="#skills"
+                                onClick={(event) => menuHighlight(event)}
+                                className="block text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white 
+                                dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Skills
+                            </a>
+                        </li>
+                        <li className="btn">
+                            <a
+                                href="#projects"
+                                onClick={(event) => menuHighlight(event)}
+                                className="block text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white 
+                                dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Works
+                            </a>
+                        </li>
+                        <li className="btn">
+                            <a
+                                href="#pastworks"
+                                onClick={(event) => menuHighlight(event)}
+                                className="block text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white 
+                                dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Past Work Ex.
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,56 +7,56 @@ function Projects() {
             <div className="grid grid-cols-1 m-8 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 
                 <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                    <img src="https://via.placeholder.com/640x360" alt="Project 1" className="w-full h-48 object-cover" />
+                    <img src="https://github.com/Sagittarius17.png" alt="Project 1" className="w-full h-48 object-cover" />
                     <div className="p-4">
                         <h3 className="text-lg font-bold mb-2">Chatroom</h3>
-                        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ante ac velit vehicula, ut pellentesque enim efficitur.</p>
+                        <p className="text-gray-700"></p>
                         <a href="https://github.com/Sagittarius17/python-chatroom" className="text-blue-600 hover:text-blue-800 mt-2 inline-block">View project on Github</a>
                     </div>
                 </div>
 
                 <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                    <img src="https://via.placeholder.com/640x360" alt="Project 2" className="w-full h-48 object-cover" />
+                    <img src="https://github.com/Sagittarius17.png" alt="Project 2" className="w-full h-48 object-cover" />
                     <div className="p-4">
                         <h3 className="text-lg font-bold mb-2">P2P Text File Transfer</h3>
-                        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ante ac velit vehicula, ut pellentesque enim efficitur.</p>
+                        <p className="text-gray-700"></p>
                         <a href="https://github.com/Sagittarius17/python-p2p-file-transfer" className="text-blue-600 hover:text-blue-800 mt-2 inline-block">View project on Github</a>
                     </div>
                 </div>
 
                 <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                    <img src="https://via.placeholder.com/640x360" alt="Project 3" className="w-full h-48 object-cover" />
+                    <img src="https://github.com/Sagittarius17.png" alt="Project 3" className="w-full h-48 object-cover" />
                     <div className="p-4">
                         <h3 className="text-lg font-bold mb-2">Python Small Projects</h3>
-                        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ante ac velit vehicula, ut pellentesque enim efficitur.</p>
-                        <a href="#" className="text-blue-600 hover:text-blue-800 mt-2 inline-block">View project on Github</a>
+                        <p className="text-gray-700"></p>
+                        <a href="https://github.com/Sagittarius17/python-small-projects" className="text-blue-600 hover:text-blue-800 mt-2 inline-block">View project on Github</a>
                     </div>
                 </div>
 
                 <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                    <img src="https://via.placeholder.com/640x360" alt="Project 4" className="w-full h-48 object-cover" />
+                    <img src="https://github.com/Sagittarius17.png" alt="Project 4" className="w-full h-48 object-cover" />
                     <div className="p-4">
-                        <h3 className="text-lg font-bold mb-2">Project 4</h3>
-                        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ante ac velit vehicula, ut pellentesque enim efficitur.</p>
-                        <a href="#" className="text-blue-600 hover:text-blue-800 mt-2 inline-block">View project</a>
+                        <h3 className="text-lg font-bold mb-2">Django eCommerce</h3>
+                        <p className="text-gray-700"></p>
+                        <a href="https://github.com/Sagittarius17/django-ecommerce" className="text-blue-600 hover:text-blue-800 mt-2 inline-block">View project on Github</a>
                     </div>
                 </div>
 
                 <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                    <img src="https://via.placeholder.com/640x360" alt="Project 4" className="w-full h-48 object-cover" />
+                    <img src="https://github.com/Sagittarius17.png" alt="Project 4" className="w-full h-48 object-cover" />
                     <div className="p-4">
-                        <h3 className="text-lg font-bold mb-2">Project 5</h3>
-                        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ante ac velit vehicula, ut pellentesque enim efficitur.</p>
-                        <a href="#" className="text-blue-600 hover:text-blue-800 mt-2 inline-block">View project</a>
+                        <h3 className="text-lg font-bold mb-2">Django Content Management System</h3>
+                        <p className="text-gray-700"></p>
+                        <a href="https://github.com/Sagittarius17/django-cms" className="text-blue-600 hover:text-blue-800 mt-2 inline-block">View project on Github</a>
                     </div>
                 </div>
 
                 <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                    <img src="https://via.placeholder.com/640x360" alt="Project 4" className="w-full h-48 object-cover" />
+                    <img src="https://github.com/Sagittarius17.png" alt="Project 4" className="w-full h-48 object-cover" />
                     <div className="p-4">
-                        <h3 className="text-lg font-bold mb-2">Project 6</h3>
-                        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ante ac velit vehicula, ut pellentesque enim efficitur.</p>
-                        <a href="#" className="text-blue-600 hover:text-blue-800 mt-2 inline-block">View project</a>
+                        <h3 className="text-lg font-bold mb-2">Django Weather App</h3>
+                        <p className="text-gray-700"></p>
+                        <a href="https://github.com/Sagittarius17/django-weather-app" className="text-blue-600 hover:text-blue-800 mt-2 inline-block">View project on Github</a>
                     </div>
                 </div>
             </div>
